06f89cd9925902583665fb2c8d810cfe96253468
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-11-06 19:27:29 +0100

    basic layout

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import path from 'path';
+const __dirname = path.resolve(path.dirname(''));
+
+const app = express();
+const port = process.env.PORT || 3011;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './public/views'));
+
+const staticDirectory = path.join(__dirname, 'public');
+app.use(express.static(staticDirectory));
+
+app.get('/', (req, res) => {
+	res.render('index', {
+		title: 'Home',
+		currentPageIdCode: 'home',
+		message: 'this is the home page'
+	});
+});
+
+app.get('/info', (req, res) => {
+	res.render('info', {
+		title: 'Info',
+		currentPageIdCode: 'info',
+		message: 'this is the info page'
+	});
+});
+
+app.listen(port, () => {
+	console.log('listening on port: http://localhost:' + port);
+});
