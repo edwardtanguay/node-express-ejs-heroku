c0d6194d6a041ed037a70e68fa063c3c7b8a8588
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-11-06 20:07:19 +0100

    add applocation

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import path from 'path';
-const __dirname = path.resolve(path.dirname(''));
+import dotenv from 'dotenv';
 
+const __dirname = path.resolve(path.dirname(''));
+dotenv.config();
 const app = express();
 const port = process.env.PORT || 3011;
 
@@ -11,8 +13,15 @@ app.set('views', path.join(__dirname, './public/views'));
 const staticDirectory = path.join(__dirname, 'public');
 app.use(express.static(staticDirectory));
 
+const siteData = {
+	applocation: process.env.APP_LOCATION
+};
+
+console.log(siteData);
+
 app.get('/', (req, res) => {
 	res.render('index', {
+		...siteData,
 		title: 'Home',
 		currentPageIdCode: 'home',
 		message: 'This is the main home page.'
@@ -21,6 +30,7 @@ app.get('/', (req, res) => {
 
 app.get('/info', (req, res) => {
 	res.render('info', {
+		...siteData,
 		title: 'Info',
 		currentPageIdCode: 'info',
 		message: 'This is the info page.'
